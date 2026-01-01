a89ee8c7895fecf4a4a5c07721064d9dfd07734e
cinderlord <103160912+HeartFaker@users.noreply.github.com>
2026-01-01 10:45:45 +0000

    feat: svm augmentation

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from model_a.download_dataset_for_test import prepare_breastmnist_dataset
-from model_a.eval_a import run_model_a_experiments
-from model_b.eval_b import run_model_b_experiments
+from A.download_dataset_for_test import prepare_breastmnist_dataset
+from A.experiment_a import run_model_a_experiments
+from B.experiment_b import run_model_b_experiments
 
 
 if __name__ == "__main__":
 
-    prepare_breastmnist_dataset()
+    # prepare_breastmnist_dataset()
     run_model_a_experiments()
-    run_model_b_experiments()
+    # run_model_b_experiments()
